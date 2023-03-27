534d7fe7912ee6a674cbfd2bd53ee9580152ccbe
iryna.chyrkova <irenchik.fart@gmail.com>
2023-03-27 20:07:42 +0300

    Таски 11

--- src/test/java/absoft/test11_methods1.java
+++ src/test/java/absoft/test11_methods1.java
@@ -9,4 +9,15 @@ Price может быть как целым числом так и дробны�
 
 public class test11_methods1 {
 
+    private static double total(double price, int qty) {
+        return price * qty * 0.8;
+    }
+
+    public static void main(String[] args) {
+        double price = 3.6;
+        int qty = 7;
+
+        System.out.println("Your total = " + total(price, qty));
+    }
+
 }
